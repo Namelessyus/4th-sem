1b9b3aa0b0aee10f2eb118e5f7e1f81ecd00fac5
Namelessyus <anushmasky@gmail.com>
2025-08-16 12:09:40 +0545

    day 3, 4 and 5

--- day_3/script.js
+++ day_3/script.js
@@ -1,32 +1,31 @@
-//te operator
-let isMember = true;
+// ternary operator
+let isMemeber = true;
 
-console.log(isMember ? '$2.00' : '$10.00' );
+console.log(isMemeber ? '$2.00' : '$10.00');
 
 let isActive = false;
 title = document.getElementById('title');
 
-isActive ? title.innerHTMl = 'I am active' : title.innerHTMl = 'I am inactive';
+isActive ? title.innerHTML = 'I am active' : title.innerHTML = 'I am inactive';
 
 demo = document.getElementById('demo');
-
-//conditional statement
-if(isActive){
-    demo.innerHTMl = "I am today's demo";
-}else{
-    demo.innerHTMl = "I am not showing today..."
+// conditional statement
+if (isActive){
+    demo.innerHTML = "I am today's demo";
+} else {
+    demo.innerHTML = "I am not showing today..."
 }
 
-//if else if statement
-let daytime = "Wednesday";
+// if else if statement
+let dayName = "Wednesday";
 day = document.getElementById('day');
 
 let dayCount = 4;
 
-if(dayName === "Wednesday" && dayCount == 4){
-    day.innerHTMl = dayName;
-}else if (dayName === "Sunday"){
-    day.innerHTMl === "Sunday";
-}else {
-    day.innerHTMl = "No day found";
+if (dayName === "Wednesday" && dayCount == 4){
+    day.innerHTML = dayName;
+} else if (dayName === "Sunday"){
+    day.innerHTML === "Sunday";
+} else {
+    day.innerHTML = "No day found";
 }
---
+++ day_5/script.js
@@ -0,0 +1,13 @@
+function square(number){
+    return number * number;
+}
+
+function cube(number){
+    return number * number * number;
+}
+
+function squareRoot(number){
+    return Math.sqrt(number);
+}
+
+console.log(square(10));
