2a8194810ffd0f8f8c81aeab39f551b794044991
Nameless <anushmasky@gmail.com>
2025-09-02 10:59:50 +0545

    arrangements

---
+++ javascript/Lab/Lab2/script.js
@@ -0,0 +1,14 @@
+console.log("Lab 2 : Array Objects and Array Methods");
+
+//defining array
+//method one: defining as data type
+
+const places = ["Dharan","Hetauda","Biratnagar","Pokhara"];
+console.log("Places of Nepal: ",places);
+
+console.log(places[0]);
+console.log(places[1]);
+console.log(places[2]);
+console.log(places[3]);
+
+
--- javascript/day_1/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello world");
-console.log("Trying git and github for the first time");
-console.log("Repo created & ssh-key generated");
-console.log("Repo cloned & code added");
-console.log("Code committed & pushed");
-console.log("Code committed & merged");
-console.log("Code committed & pulled");
-console.log("Code committed & merged");
