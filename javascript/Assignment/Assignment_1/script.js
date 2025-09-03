55cc21eace5af62caa7c0eb2164fc3c56f9cefc6
Namelessyus <anushmasky@gmail.com>
2025-09-03 23:36:16 +0545

    assignment updates

---
+++ javascript/Assignment/Assignment_1/script.js
@@ -0,0 +1,25 @@
+console.log("Javascript Assignment: Grocery Store Billing System");
+
+//1. Display a list of available products (name, price, stock).
+console.log("List of Available Products:");
+console.log("1. Rice  - Rs.120  Stock: 10");
+console.log("2. Sugar - Rs.80   Stock: 15");
+console.log("3. Milk  - Rs.50   Stock: 20");
+console.log("4. Bread - Rs.40   Stock: 8");
+console.log("5. Eggs  - Rs.10   Stock: 30");
+
+//2. Provide menu options:
+//- View Products
+//- Add to Cart
+//- Remove from Cart
+//- View Cart
+//- Checkout
+//- Exit
+
+console.log("Menu");
+console.log("View Products");
+console.log("Add to Cart");
+console.log("Remove from Cart");
+console.log("View Cart");
+console.log("Checkout");
+console.log("Exit");
