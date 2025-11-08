c4d5404042a601b68bfb0fc7a7ab0fd5ec50be2a
Namelessyus <anushmasky@gmail.com>
2025-11-08 10:58:13 +0545

    lab 2 Newton Raphson (NR) Method complete

--- Numerical method/Anushma Shakya Lab2 Newton Raphson.cpp
+++ Numerical method/Anushma Shakya Lab2 Newton Raphson.cpp
@@ -1,27 +1,45 @@
 // Newton Raphson (NR) Method
 	    #include<stdio.h>
         #include<math.h>
+#include<stdlib.h>
         
-        #define f(x) 3x-cos(x)-1
+        #define f(x) (3*x-cos(x)-1)
         #define g(x) 3+sin(x)
         
         int main(){
-        	float x0,x1,x2,f0,f1,f2,e,n;
-        	
-        	printf("enter initial guess");
-        	printf("\n input x1: ");
-        	scanf("%f",&x1);
-        	printf("\n input x2: ");
-        	scanf("%f",&x2);   
-        	
-        	printf("\n input tolerable error : ");
-        	scanf("%f",&e);
-        	
-        	printf("\n input Maximum Iteration : ");
-        	scanf("%f",&n);
-        	
-        do {
-        if(g(x0)==0)
-        printf("Mathematical Error");
+			    float x0, x1, e, f0,g0;
+			    int n, i = 1;        
+					
+		  printf("Enter initial guess: ");
+		    scanf("%f", &x0);
+		
+		    printf("Enter tolerable error: ");
+		    scanf("%f", &e);
+		
+		    printf("Enter maximum iterations: ");
+		    scanf("%d", &n);
+		
+			do{
+				f0=f(x0);
+				g0=g(x0);
+				if(g0==0){
+					printf("mathematical error");
+					exit(0);	
+				}
+				x1=x0-f0/g0;
+				x0=x1;
+				i=i+1;
+				if(i>n){
+					printf("not convergent");
+					exit(0);
+				}
+			}while((fabs f(x1))>e);
+			printf("root= %f", x1);
+			
+			return 0;
 		}
-        
+		
+		
+
+		
+		
