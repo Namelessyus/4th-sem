e19020ade731e1db6909f43f40371dae36c051f4
Namelessyus <anushmasky@gmail.com>
2025-12-19 19:44:40 +0545

    insert and select

--- javascript/Classes/day_9/SL_PHP_CRUD/dbconnection.php
+++ javascript/Classes/day_9/SL_PHP_CRUD/dbconnection.php
@@ -1,9 +1,12 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = ""; //put password blank if you are using mysql in XAMP
+$password = "1234"; // put password blank if you are using
+// mysql in XAMPP
 $db_name = "php_crud_example";
-$connect = mysqli_connect ($host,$user,$password,$db_name);
-if(connect){
-    die("connection failed:".mysqli_connct_error());
+
+$connect = mysqli_connect($host, $user, $password, $db_name);
+
+if (!$connect){
+    die("Connection failed: " . mysqli_connect_error());
 }
--- javascript/Classes/day_9/SL_PHP_CRUD/select_example.php
+++ javascript/Classes/day_9/SL_PHP_CRUD/select_example.php
@@ -1,19 +1,21 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = ""; //put password blank if you are using mysql in XAMP
+$password = "1234"; // put password blank if you are using
+// mysql in XAMPP
 $db_name = "php_crud_example";
-$connect = mysqli_connect ($host,$user,$password,$db_name);
-if(!$connect){
-    die("connection failed:".mysqli_connect_error());
-} else{
-    echo"Database connected sucessfully <br>";
 
-    $query = "SELECT * FROM product;";
+$connect = mysqli_connect($host, $user, $password, $db_name);
+
+if (!$connect){
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Database connected succsessfully<br>";
+
+    $query = "SELECT * FROM product";
     
     $result = mysqli_query($connect, $query);
-
-    if(mysqli_num_rows($result)>0){
+    if(mysqli_num_rows($result) > 0){
         while($data = mysqli_fetch_array($result)){
             echo "Title: {$data['product_title']}<br>";
             echo "Category: {$data['product_category']}<br>";
@@ -21,7 +23,7 @@ if(!$connect){
             echo "Description: {$data['product_description']}<br>";
             echo "Quantity: {$data['product_quantity']}<br><br>";
         }
-    }else{
-        echo "No records found";
+    } else {
+        echo "No records found.";
     }
-?>
+}
